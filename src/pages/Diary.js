d4389fafc2abc6405cfaaba35bcaa759be39211f
sanghyup <sh.lee@dc-on.co.kr>
2023-09-05 17:43:01 +0900

    페이지 라우팅 2- React Router 응용까지

---
+++ src/pages/Diary.js
@@ -0,0 +1,13 @@
+import { useParams } from "react-router-dom";
+
+const Diary = () => {
+  const { id } = useParams();
+  return (
+    <div>
+      <h1>Diary</h1>
+      <p>이곳은 다이어리 상세입니다.</p>
+    </div>
+  );
+};
+
+export default Diary;
---
+++ src/pages/New.js
@@ -0,0 +1,10 @@
+const New = () => {
+  return (
+    <div>
+      <h1>NEW</h1>
+      <p>이곳은 일기작성페이지입니다.</p>
+    </div>
+  );
+};
+
+export default New;
